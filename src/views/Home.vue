ee09a2b30e01074a6d3afb5153b9c8b66121cd44
Jun Nishimura <n.junjun0303@gmail.com>
2021-09-11 09:53:54 +0900

    Fix components path to import

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,13 +25,13 @@
 </template>
 
 <script>
-import Landing from '@/components/Landing.vue'
-import Concept from '@/components/Concept.vue'
-import About from '@/components/About.vue'
-import Projects from '@/components/Projects.vue'
-import News from '@/components/News.vue'
-import Gallery from '@/components/Gallery.vue'
-import PageFooter from '@/components/PageFooter.vue'
+import Landing from '@/components/sections/Landing.vue'
+import Concept from '@/components/sections/Concept.vue'
+import About from '@/components/sections/About.vue'
+import Projects from '@/components/sections/Projects.vue'
+import News from '@/components/sections/News.vue'
+import Gallery from '@/components/sections/Gallery.vue'
+import PageFooter from '@/components/sections/PageFooter.vue'
 
 export default {
   components: {
