f16cba5f80700cb56ab3a1a462c1c08e16a0fb5b
Jun Nishimura <n.junjun0303@gmail.com>
2021-09-11 10:53:26 +0900

    Add section header #7

---
+++ src/components/common/SectionHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class='section-header'>
+    <div class='section-title'>
+      {{ sectionTitle }}
+    </div>
+  </header>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const sectionTitle = computed(() => props.title.toUpperCase())
+
+    return { sectionTitle }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+.section {
+  &-header {
+    margin-bottom: 2rem;
+  }
+
+  &-title {
+    display: inline-block;
+    font-size: $font-size-section-header;
+    font-weight: bold;
+    border-bottom: 2px solid #fff;
+  } 
+}
+</style>
--- src/components/sections/News.vue
+++ src/components/sections/News.vue
@@ -1,21 +1,25 @@
 <template>
   <wrapper>
     <div class='news'>
-      <h1>news</h1>
+      <section-header title="news" />
     </div>
   </wrapper>
 </template>
 
 <script>
 import Wrapper from '@/components/common/Wrapper.vue'
+import SectionHeader from '@/components/common/SectionHeader.vue'
 export default {
   components: { 
-    Wrapper 
+    Wrapper,
+    SectionHeader 
   },
   setup() {}
 }
 </script>
 
 <style scoped lang='scss'>
-
+.news {
+  text-align: center;
+}
 </style>
