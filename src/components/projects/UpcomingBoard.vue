df847ca38dc150733de7fa3a9355895ca0a8e1e0
Jun Nishimura <n.junjun0303@gmail.com>
2021-09-12 20:14:03 +0900

    Add real timer #9

--- src/components/projects/UpcomingBoard.vue
+++ src/components/projects/UpcomingBoard.vue
@@ -4,12 +4,47 @@
       <div class="upcoming-board__sentence">
         公開まで
         <div class="upcoming-board__time">
-          23日13時間41分58秒
+          {{ displayTime }}
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { ref, onMounted, computed } from 'vue'
+export default {
+  setup() {
+    onMounted(() => {
+      setInterval(updateTimer, 1000)
+    })
+
+    const day = ref(0)
+    const hour = ref(0)
+    const min = ref(0)
+    const sec = ref(0)
+    
+    const updateTimer = (() => {
+      const curTime = new Date()
+      const targetTime = new Date('2021-11-06T00:00:00')
+      const diff = targetTime.getTime() - curTime.getTime()
+      day.value = diff / (1000 * 60 * 60 * 24)
+      hour.value = (day.value - Math.floor(day.value)) * 24
+      min.value = (hour.value - Math.floor(hour.value)) * 60
+      sec.value = (min.value - Math.floor(min.value)) * 60
+    })
+
+    const displayTime = computed(() => {
+      return Math.floor(day.value).toString() + '日'
+        + Math.floor(hour.value).toString() + '時間'
+        + Math.floor(min.value).toString() + '分'
+        + Math.floor(sec.value).toString() + '秒'
+    })
+    
+    return { displayTime }
+  }
+}
+</script>
 
 <style scoped lang="scss">
 .upcoming-board {
