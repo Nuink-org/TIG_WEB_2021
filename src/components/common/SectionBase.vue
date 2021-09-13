8da8f6676bc42bd8990386bc7e0db3cc6bbce53d
Jun Nishimura <n.junjun0303@gmail.com>
2021-09-13 19:04:16 +0900

    Add about section #13

--- src/components/common/SectionBase.vue
+++ src/components/common/SectionBase.vue
@@ -1,10 +1,12 @@
 <template>
   <wrapper :isFull="isWrapperFull" class="section-base">
-    <div v-if="isTitleNeeded" class="section-base__header">
-      <section-header :title="title" />
-    </div>
-    <div class="section-base__content">
-      <slot name="content" />
+    <div class="section-base__wrapper" :class="{'section-base__wrapperRight': isRight}">
+      <div v-if="isTitleNeeded" class="section-base__header">
+        <section-header :title="title" />
+      </div>
+      <div class="section-base__content">
+        <slot name="content" />
+      </div>
     </div>
   </wrapper>
 </template>
@@ -26,7 +28,11 @@ export default {
     title: {
       type: String,
       default: ''
-    }
+    },
+    isRight: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props) {
     const isTitleNeeded = computed(() => props.title !== '')
@@ -39,6 +45,14 @@ export default {
 <style scoped lang='scss'>
 .section-base {
   text-align: center;
+  display: flex;
+  &__wrapper {
+    width: 100%
+  }
+  &__wrapperRight {
+    width: 40%;
+    margin-left: auto;
+  }
   &__header {
     margin-bottom: 2rem;
   }
--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -1,21 +1,84 @@
 <template>
-  <wrapper>
-    <div class="about">
-      <h1>about</h1>
-    </div>
-  </wrapper>
+  <section-base
+    :isWrapperFull="false"
+    :isRight="true"
+    title="about"
+    class="about"
+    :style="imgPadding"
+  >
+    <template #content>
+      <div class="about-image">
+        <img src="@/assets/images/about.jpeg" ref="imageRef">
+      </div>
+      <div class="about-sentence">
+        <div v-for="sentence in sentences"
+          :key="sentence"
+          class="about-paragraph"
+        >
+          {{ sentence }}
+        </div>
+      </div>
+    </template>
+  </section-base>
 </template>
 
 <script>
-import Wrapper from '@/components/common/Wrapper.vue'
+import { computed, onMounted, ref } from 'vue'
+import SectionBase from '@/components/common/SectionBase.vue'
 export default {
   components: {
-    Wrapper,
+    SectionBase
   },
-  setup() {}
+  setup() {
+    const imageRef = ref(null)
+    const imageHeight = ref(0)
+
+    onMounted(() => {
+      // 画像が読み込まれた後に高さを測る
+      imageRef.value.addEventListener('load', () => {
+        imageHeight.value = imageRef.value.clientHeight
+      })
+    })
+
+    const imgPadding = computed(() => {
+      // 画像の位置をabsoluteで指定しているため、余分にpadding-bottomする必要がある
+      const paddingAmount = imageHeight.value * 0.618
+      return {'padding-bottom': paddingAmount.toString() + 'px'}
+    })
+
+    const sentences = [
+      'Innovation World Festa (通称イノフェス)は筑波大学とラジオ局J-WAVEが主催する、テクノロジーと音楽のクリエイティブフェスです。',
+      '例年は J-WAVE と共催していましたが今年はNu ink.単独で、イノフェスのサテライトイベントとして「Tsukuba Innovation Gallery」を開催します。'
+    ]
+
+    return { imageRef, imgPadding, sentences }
+  }
 }
 </script>
 
 <style scoped lang='scss'>
+.about {
+  margin-top: 4rem;
+  margin-left: auto;
+  position: relative;
+  &-image {
+    width: 1000px;
+    position: absolute;
+    top: -80px;
+    left: -200px;
+    z-index: -1;
 
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+  &-sentence {
+    margin-top: 4rem;
+    line-height: 2rem;
+  }
+  &-paragraph + &-paragraph {
+    margin-top: 3rem;
+  }
+}
 </style>
