b2fac98d0907eda7c04a0e26cc6048ad4e1f95a2
Jun Nishimura <n.junjun0303@gmail.com>
2021-09-17 15:25:47 +0900

    Add noisy sphere #21

--- src/components/three/Drawing.vue
+++ src/components/three/Drawing.vue
@@ -1,29 +1,41 @@
 <template>
   <renderer ref="renderer" antialias resize="window">
     <camera :position="{ z: 10 }" />
-    <scene>
+    <scene ref="scene">
       <point-light :position="{ y: 50, z: 50 }" />
-      <box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
+      <noisy-sphere ref="sphere"
+        :radius="2"
+        :widthSegments="64"
+        :heightSegments="64"
+        :timeCoef="0.0006"
+        :noiseCoef="0.35"
+        :dispCoef="1.1"
+      >
         <lambert-material />
-      </box>
+      </noisy-sphere>
     </scene>
   </renderer>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
+import NoisySphere from 'troisjs/src/components/noisy/NoisySphere.js'
+import { Renderer, Camera, Scene, PointLight, LambertMaterial } from 'troisjs'
 export default {
+  components: {
+    NoisySphere,
+    Renderer,
+    Camera,
+    Scene,
+    PointLight,
+    LambertMaterial,
+  },
   setup() {
     const renderer = ref(null)
-    const box = ref(null)
+    const scene = ref(null)
+    const sphere = ref(null)
 
-    onMounted(() => {
-      renderer.value.onBeforeRender(() => {
-        box.value.mesh.rotation.x += 0.01
-      })
-    })
-
-    return { renderer, box}
+    return { renderer, scene, sphere }
   }
 }
 </script>
