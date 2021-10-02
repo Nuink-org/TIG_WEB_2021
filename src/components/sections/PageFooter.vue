9183becd8df406be82136fd785790671cec6b630
shaunnnaka <66339191+shaunnnaka@users.noreply.github.com>
2021-10-02 10:14:23 +0900

    Adjust logo spacing

--- src/components/sections/PageFooter.vue
+++ src/components/sections/PageFooter.vue
@@ -5,25 +5,30 @@
     <template #content>
       <div class="footer-bg">
         <box class="page-footer">
-          <img src="@/assets\images\Nu-logo-02.png" class="logo-nuink">
+          <div>
+            <img src="@/assets\images\Nu-logo-02.png" class="logo-nuink">
+          </div>
           <div>
             <div class="footer-upper">
               <h3 class="right-up-child">筑波大学学生団体 Nu ink.</h3>
-              
-              <a class="right-up-child web_link" href="https://nuink-tsukuba.wixsite.com/innovation-world" target="_blank" rel="noopener noreferrer"><p >official website</p> </a>
-
-              <a href="https://twitter.com/nuinktsukuba"  target="_blank" rel="noopener noreferrer">
-              <img src="@/assets\images\Twitter-logo.png" class="logos logo-tw" >
-              </a>
-              <div class="between-logos"></div>
-              <a href="https://www.instagram.com/nuinktsukuba/?hl=ja" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets\images\Instagram-logo.png" class="logos ">
-              </a>
-              <div class="between-logos"></div>
+              <div>
+                <a class="dist web_link" href="https://nuink-tsukuba.wixsite.com/innovation-world" target="_blank" rel="noopener noreferrer"><p >official website</p> </a>
+              </div>
+              <div class="dist">
+                <a href="https://twitter.com/nuinktsukuba"  target="_blank" rel="noopener noreferrer">
+                <img src="@/assets\images\Twitter-logo.png" class="logos logo-tw" >
+                </a>
+              </div>
+              <div class="dist">
+                <a href="https://www.instagram.com/nuinktsukuba/?hl=ja" target="_blank" rel="noopener noreferrer">
+                  <img src="@/assets\images\Instagram-logo.png" class="logos ">
+                </a>
+              </div>
+              <div >
               <a href="https://www.youtube.com/channel/UCCthlOqY7CCTtZ4gzQDvzmg/featured" target="_blank" rel="noopener noreferrer">
                 <img src="../../assets\images\YouTube-logo.png" class="logos logo-yt">
               </a>
-              <div class="between-logos"></div>
+              </div>
             </div>
             <div class="description">
               <p>筑波大学とラジオ局J-WAVEが主催する、テクノロジーと音楽のクリエイティブフェス INNOVATION WORLD FESTA (イノフェス)の筑波大学学生運営チーム。普段なら交わらない"人"や"学問"をつないでイノベーションを起こすという目標を掲げて活動中！</p>
@@ -56,14 +61,13 @@ export default {
   .page-footer{
     color: #000;
     display: inline-flex;
-    text-align: center;
   }
   .footer-upper{
     display: flex;
     margin-top: 30px;
   }
   .right-up-child{
-    margin-right: 70px;
+    margin-right: 135px;
   }
   .web_link{
     color: #000;
@@ -74,15 +78,17 @@ export default {
     margin-top: 8px;
   }
   .logo-nuink{
-    width: 241px;
-    height: 150px;
+    width: 289px;
+    height: 180px;
+    margin-top: -5px;
   }
   .logos{
     width: 30px;
     height: 30px;
+    
   }
-  .between-logos{
-    margin-right: 20px;
+  .dist{
+    margin-right: 25px;
   }
   .logo-tw{
     border-radius: 6px;
