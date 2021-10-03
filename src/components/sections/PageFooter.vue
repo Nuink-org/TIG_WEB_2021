f2116fcd115f3a00df66c1c518ac467422c86cc9
Jun Nishimura <n.junjun0303@gmail.com>
2021-10-03 19:53:44 +0900

    make footer responsive #49

--- src/components/sections/PageFooter.vue
+++ src/components/sections/PageFooter.vue
@@ -4,113 +4,176 @@
     class="pagefooter"  
   >
     <template #content>
-      <div class="footer-bg">
-        <box class="page-footer">
-          <div>
-            <img src="@/assets\images\Nu-logo-02.png" class="logo-nuink">
-          </div>
-          <div>
-            <div class="footer-upper">
-              <h3 class="right-up-child">筑波大学学生団体 Nu ink.</h3>
-              <div>
-                <a class="dist web_link" href="https://nuink-tsukuba.wixsite.com/innovation-world" target="_blank" rel="noopener noreferrer"><p >official website</p> </a>
-              </div>
-              <div class="dist">
-                <a href="https://twitter.com/nuinktsukuba"  target="_blank" rel="noopener noreferrer">
-                <img src="@/assets\images\Twitter-logo.png" class="logos logo-tw" >
-                </a>
-              </div>
-              <div class="dist">
-                <a href="https://www.instagram.com/nuinktsukuba/?hl=ja" target="_blank" rel="noopener noreferrer">
-                  <img src="@/assets\images\Instagram-logo.png" class="logos ">
-                </a>
-              </div>
-              <div >
-              <a href="https://www.youtube.com/channel/UCCthlOqY7CCTtZ4gzQDvzmg/featured" target="_blank" rel="noopener noreferrer">
-                <img src="@/assets/images/YouTube-logo.png" class="logos logo-yt">
-              </a>
-              </div>
-            </div>
-            <div class="description">
-              <p>筑波大学とラジオ局J-WAVEが主催する、テクノロジーと音楽のクリエイティブフェス INNOVATION WORLD FESTA (イノフェス)の筑波大学学生運営チーム。普段なら交わらない"人"や"学問"をつないでイノベーションを起こすという目標を掲げて活動中！</p>
-            </div>
-          </div>
-        </box>
-        <div class="copyright">
-          <p>©2021. Nu ink. All rights reserved.</p>
+      <div class="pagefooter-content">
+        <div class="pagefooter-logo">
+          <img src="@/assets/images/Nu-logo-02.png">
         </div>
+        <div class="pagefooter-overview">
+          <div class="pagefooter-overview__header">
+            <div class="pagefooter-overview__orgName">
+              筑波大学学生団体<br v-if="isResponsive"><span v-if="!isResponsive">&nbsp;</span><span class="nu">Nu</span>&nbsp;<span class="ink">ink.</span>
+            </div>
+            <div class="pagefooter-overview__website">
+              <a href="https://nuink-tsukuba.wixsite.com/innovation-world" target="_blank" rel="noopener noreferrer">
+                official website
+              </a>
+            </div>
+            <div class="pagefooter-overview__twitter">
+              <a href="https://twitter.com/nuinktsukuba" target="_blank" rel="noopener noreferrer">
+                <img src="@/assets/images/Twitter-logo.png">
+              </a>
+            </div>
+            <div class="pagefooter-overview__instagram">
+              <a href="https://www.instagram.com/nuinktsukuba/?hl=ja" target="_blank" rel="noopener noreferrer">
+                <img src="@/assets/images/Instagram-logo.png">
+              </a>
+            </div>
+            <div class="pagefooter-overview__youtube">
+              <a href="https://www.youtube.com/channel/UCCthlOqY7CCTtZ4gzQDvzmg/featured" target="_blank" rel="noopener noreferrer">
+                <img src="@/assets/images/YouTube-logo.png">
+              </a>
+            </div>
+          </div>
+          <div class="pagefooter-description">
+            筑波大学とラジオ局J-WAVEが主催する、テクノロジーと音楽のクリエイティブフェス INNOVATION WORLD FESTA （イノフェス）の筑波大学学生運営チーム。普段なら交わらない「人」や「学問」をつないでイノベーションを起こすという目標を掲げて活動中！
+          </div>
+        </div>
+      </div>
+      <div class="pagefooter-copyright">
+        ©2021.&nbsp;<span class="nu">Nu</span>&nbsp;<span class="ink">ink.</span>&nbsp;All rights reserved.
       </div>
     </template>
   </section-base>
 </template>
 
 <script>
-import SectionBase from '../common/SectionBase.vue'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import SectionBase from '@/components/common/SectionBase.vue'
 export default {
   components: { 
     SectionBase 
+  },
+  setup() {
+    const store = useStore()
+    const isResponsive = computed(() => store.state.isResponsivePhone)
+
+    return { isResponsive }
   }
 }
 </script>
 
 <style scoped lang='scss'>
-  .footer-bg{
-    background: #fff;
-    height: 300px;
-    padding-top: 50px;
-  }
-  .page-footer{
+.pagefooter{
+  background: #fff;
+  min-height: 266px;
+  padding-top: 0 !important;
+  position: relative;
+  text-align: left;
+  &-content, &-copyright {
     color: #000;
-    display: inline-flex;
   }
-  .footer-upper{
+  &-content {
+    font-family: $font-family-jp;
     display: flex;
-    padding-top: 50px;
+    justify-content: center;
+    align-items: center;
+    @include respond(tablet) {
+      flex-direction: column;
+    }
   }
-  .right-up-child{
-    margin-right: 230px;
+  &-logo {
+    width: 256px;
+    height: 256px;
+    position: relative;
+    img {
+      width: 100%;
+      height: auto;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
   }
-  .web_link{
-    color: #000;
-    text-decoration: none;
-    display: block;
-    height: 0px;
-    background: #f00;
-    margin-top: 4px;
+  &-overview {
+    max-width: 600px;
+    @include respond(tablet) {
+      max-width: 480px;
+      min-height: 200px;
+    }
+    @include respond(phone) {
+      width: 80%;
+    }
+    &__header {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+    }
+    &__orgName {
+      font-size: $font-size-footer-orgName;
+      margin-right: auto;
+      .ink {
+        font-size: 1.1em;
+      }
+      @include respond(phone) {
+        font-size: 0.98em;
+      }
+    }
+    &__website, &__twitter, &__instagram {
+      margin-right: 0.5rem;
+    }
+    &__website {
+      a {
+        color: #000;
+        text-decoration: none;
+        @include respond(phone) {
+          font-size: 0.618em;
+        }
+      }
+    }
+    &__twitter, &__instagram, &__youtube {
+      width: 32px;
+      height: 32px;
+      position: relative;
+      a {
+        width: 100%;
+        height: auto;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        img {
+          width: 100%;
+          height: auto;
+          border-radius: 15%;
+        }
+      }
+    }
   }
-  .logo-nuink{
-    width: 337px;
-    height: 210px;
+  &-description{
+    color: #222;
+    font-size: $font-size-footer-description;
+    margin-top: 1.4rem;
   }
-  .logos{
-    width: 30px;
-    height: 30px;
-    
+  &-copyright{
+    color: #111;
+    font-size: $font-size-footer-copyright;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    .ink {
+      font-size: 1.1em;
+    }
   }
-  .dist{
-    margin-right: 25px;
+  .nu {
+    font-weight: bold;
+    font-family: $font-family-nuink-nu;
   }
-  .logo-tw{
-    border-radius: 7px;
-    border-color: #fff;
+  .ink {
+    font-weight: bold;
+    letter-spacing: 1.5px;
+    font-family: $font-family-nuink-ink;
   }
-  .logo-yt{
-    width: 40px;
-    height: 40px;
-    margin-top: -4px;
-    margin-left: -5px;
-  }
-  .description{
-    width: 750px;
-    margin-right: 100px;
-    text-align: justify;
-    letter-spacing: -0.08em;
-  }
-  .copyright{
-    clear: both;
-    text-align: center;
-    color: #000;
-    margin-top: 10px;
-    padding-bottom: 50px;
-  }
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,20 @@ import { createStore } from 'vuex'
 export default createStore({
   state() {
     return {
-      landingAnimCompleted: false
+      isLandingAnimCompleted: false,
+      isResponsiveTablet: false,
+      isResponsivePhone: false
     }
   },
   mutations: {
     completeLandingAnim(state) {
-      state.landingAnimCompleted = true
+      state.isLandingAnimCompleted = true
+    },
+    addResponsivenessTablet(state, payload) {
+      state.isResponsiveTablet = payload.width <= 960 ? true : false // 960はタブレット閾値
+    },
+    addResponsivenessPhone(state, payload) {
+      state.isResponsivePhone = payload.width <= 560 ? true : false
     }
   },
   actions: {
