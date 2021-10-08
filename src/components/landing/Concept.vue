df0b746a18966371befc1357930beda2f31d27b3
Jun Nishimura <n.junjun0303@gmail.com>
2021-10-08 20:13:11 +0900

    update concept #49

---
+++ src/components/landing/Concept.vue
@@ -0,0 +1,215 @@
+<template>
+  <div class="concept">
+    <div class="concept-title">
+      <div v-for="(word, idx) in conceptTitle"
+        :key="`word${idx}`"
+        :class="`concept-title__word concept-title__word${idx}`"
+      >
+        {{ word }}
+      </div>
+    </div>
+    <div class="concept-subTitle">
+      <div v-for="(word, idx) in conceptSubTitle"
+        :key="`word${idx}`"
+        :class="`concept-subTitle__word concept-subTitle__word${idx}`"
+      >
+        {{ word }}  
+      </div>
+    </div>
+    <div class="concept-description">
+      <div class="concept-description__wrapper" v-for="(sentence, idx) in conceptSentences" :key="sentence">
+        <div :class="`concept-description__sentence concept-description__sentence${idx}`">
+          <div v-for="(word, idx2) in sentence"
+            :key="`word${idx2}`"
+            :class="`concept-description__word concept-description__word${idx}-${idx2}`"
+          >
+            {{ word }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue'
+import { onMounted } from 'vue'
+import baffle from 'baffle'
+import { gsap } from 'gsap'
+import { CSSRulePlugin } from 'gsap/CSSRulePlugin'
+gsap.registerPlugin(CSSRulePlugin)
+export default {
+  setup(props, context) {
+    const isSentenceRevealed = ref(false)
+    const isAllRevealed = ref(false)
+    const conceptTitle = 'New Normal'
+    const conceptSubTitle = '～新たなありかたの模索～'
+    const conceptSentences = ["コロナ禍において", "不要不急と切り捨てられてきた", "エンタメや文化・芸術が", "新しい日常においてどうあるべきかを", "テクノロジーの視点から", "見つめ直し、想像し、", "体験できる場を創出する"]
+    let conceptTitleBaffle, conceptSubTitleBaffle, conceptSentenceBaffle
+    const duration1 = 1
+    const duration2 = 0.18
+
+    const startTitleAnimaiton = () => {
+      for (let i = 0; i < conceptTitle.length; i++) {
+        gsap.to(`.concept-title__word${i}`, {
+          opacity: 1, 
+          delay: 1 + Math.random() * 0.618,
+          ease: 'linear',
+          duration: duration1
+        })
+      }
+      for (let i = 0; i < conceptSubTitle.length; i++) {
+        if (i === conceptSubTitle.length-1) {
+          gsap.to(`.concept-subTitle__word${i}`, {
+            opacity: 1, 
+            delay: 2 + Math.random() * 0.618,
+            ease: 'linear',
+            duration: duration1,
+            onComplete: () => {
+              isAllRevealed.value = true
+            }
+          })
+        } else {
+          gsap.to(`.concept-subTitle__word${i}`, {
+            opacity: 1, 
+            delay: 2 + Math.random() * 0.618,
+            ease: 'linear',
+            duration: duration1,
+          })
+        }
+      }
+    }
+
+    const startDescriptionAnimation = () => {
+      gsap.to('.concept-description__word', {
+        opacity: 1,
+        ease: 'linear',
+        delay: 1.5,
+        stagger: 0.1,
+        duration: duration2,
+        onComplete: () => {
+          isSentenceRevealed.value = true
+        }
+      })
+    }
+
+    const initBaffle = () => {
+      conceptTitleBaffle = baffle('.concept-title__word', { speed: 1.2 })
+      conceptSubTitleBaffle = baffle('.concept-subTitle__word', { speed: 1.2 })
+      conceptSentenceBaffle = baffle('.concept-description__word', { speed: 1.2 })
+    }
+    const startObfuscating = () => {
+      conceptTitleBaffle.start()
+      conceptSubTitleBaffle.start()
+      conceptSentenceBaffle.start()
+    }
+    const stopObfuscating = () => {
+      setTimeout(() => {
+        conceptTitleBaffle.stop()
+        conceptSubTitleBaffle.stop()   
+        conceptSentenceBaffle.stop()
+        context.emit('finishConceptAnim')
+      }, 2000)
+    }
+
+    const vanishDuration = 0.06
+    const startVanishAnimation = () => {
+      setTimeout(() => {
+        startObfuscating()
+        for (let i = 0; i < conceptTitle.length; i++) {
+          gsap.to(`.concept-title__word${i}`, {
+            opacity: 0, 
+            delay: Math.random() * 0.618,
+            ease: 'linear',
+            duration: vanishDuration
+          })
+        }
+        for (let i = 0; i < conceptSubTitle.length; i++) {
+          gsap.to(`.concept-subTitle__word${i}`, {
+            opacity: 0, 
+            delay: Math.random() * 0.618,
+            ease: 'linear',
+            duration: vanishDuration,
+          })
+        }
+        for (let i = 0; i < conceptSentences.length; i++) {
+          for (let j = 0; j < conceptSentences[i].length; j++) {
+            // 最後だけにstopObfuscatingを呼ばせる
+            if (i === conceptSentences.length-1 && j === conceptSentences[i].length-1) {
+              gsap.to(`.concept-description__word${i}-${j}`, {
+                opacity: 0, 
+                delay: Math.random() * 0.618,
+                ease: 'linear',
+                duration: vanishDuration,
+                onComplete: () => {
+                  stopObfuscating()
+                }
+              })  
+            } else {
+              gsap.to(`.concept-description__word${i}-${j}`, {
+                opacity: 0, 
+                delay: Math.random() * 0.618,
+                ease: 'linear',
+                duration: vanishDuration,
+              })
+            }
+          }
+        }
+      }, 2000)
+    }
+
+    onMounted(() => {
+      initBaffle()
+      startDescriptionAnimation()
+    })
+
+    watch(
+      [isSentenceRevealed, isAllRevealed],
+      ([newIsSentenceRevealed, oldIsSentenceRevealed], [newIsAllRevealed, oldIsAllRevealed]) => {
+        if (newIsSentenceRevealed && !oldIsSentenceRevealed) {
+          startTitleAnimaiton()
+        } else if (newIsAllRevealed && !oldIsAllRevealed) {
+          startVanishAnimation()
+        }
+      }
+    )
+    return { isAllRevealed, conceptTitle, conceptSubTitle, conceptSentences }
+  }
+}
+</script>
+
+<style lang='scss'>
+.concept {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: #fff;
+  &-title, &-subTitle, &-description {
+    &__word {
+      display: inline-block;
+      min-width: 15px;
+      opacity: 0;
+    }
+  }
+  &-title {
+    font-family: $font-family-standard;
+    font-size: $font-size-concept-title;
+    font-weight: bold;
+  }
+  &-subTitle {
+    margin-top: 0.5rem;
+    font-size: $font-size-concept-subTitle;
+  }
+  &-description {
+    margin-top: 2rem;
+    line-height: 3rem;
+    &__wrapper {
+      margin: 0.6rem;
+      display: block
+    }
+  }
+}
+</style>
--- src/components/sections/Landing.vue
+++ src/components/sections/Landing.vue
@@ -1,176 +1,48 @@
 <template>
-  <div class="landing">
-    <!-- <div class="v-line" /> -->
-    <div class="title">
-      <div class="title-tsukuba">
-        <span>T</span>
-        <span class="title-hidden__tsukuba" v-for="(t, i) in revealTitles[0]"
-          :key="i"
-        >
-          {{ t }}
-        </span>
-      </div>
-      <div class="title-innovation">
-        <span>I</span>
-        <span class="title-hidden__innovation" v-for="(t, i) in revealTitles[1]"
-          :key="i"
-        >
-          {{ t }}
-        </span>
-      </div>
-      <div class="title-gallery">
-        <span>G</span>
-        <span class="title-hidden__gallery" v-for="(t, i) in revealTitles[2]"
-          :key="i"
-        >
-          {{ t }}
-        </span>
-      </div>
-    </div>
-    <div class="date">
-      2021.11.6-7
-    </div>
-    <div class="organizer">
-      <div class="presents">
-        presented by
-      </div>
-      <div class="nu-ink">
-        <span class="nu">Nu</span>&nbsp;<span class="ink">ink.</span>
-      </div>
-    </div>
-  </div>
+  <section-base
+    :isWrapperFull="true"
+    class="landing"
+  >
+    <template #content>
+      <concept 
+        v-if="!isConceptAnimDone"
+        @finishConceptAnim="startTitleAnimation" 
+      />
+      <title-screen
+        v-if="isConceptAnimDone"
+        :isConceptAnimDone="isConceptAnimDone"
+      />
+    </template>
+  </section-base>
 </template>
 
 <script>
-import { onMounted } from 'vue'
-import { useStore } from 'vuex'
-import gsap from 'gsap'
+import { ref } from 'vue'
+import SectionBase from '@/components/common/SectionBase.vue'
+import Concept from '@/components/landing/Concept.vue'
+import TitleScreen from '@/components/landing/TitleScreen.vue'
 export default {
+  components: {
+    SectionBase,
+    Concept,
+    TitleScreen
+  },
   setup() {
-    // タイトルで後から現れてくる部分 
-    const revealTitles = [Array.from("SUKUBA"), Array.from("NNOVATION"), Array.from("ALLERY")] 
-    const initDelay = 3
-    const store = useStore()
+    const isConceptAnimDone = ref(false)
 
-    // animation
-    onMounted(() => {
-      gsap.from('.title-tsukuba', {
-        duration: 2.5,
-        delay: initDelay,
-        left: '48%',
-        ease: 'expo.out'
-      })
-      gsap.from('.title-innovation', {
-        duration: 2.5,
-        delay: initDelay,
-        left: '49%',
-        ease: 'expo.out'
-      })
-      gsap.from('.title-gallery', {
-        duration: 2.5,
-        delay: initDelay, 
-        left: '48%',
-        ease: 'expo.out'
-      })
-      gsap.from('.title-hidden__tsukuba', {
-        duration: 2.5,
-        delay: initDelay,
-        opacity: 0,
-        stagger: 0.1,
-        ease: 'expo.out'
-      })
-      gsap.from('.title-hidden__innovation', {
-        duration: 2.5,
-        delay: initDelay,
-        opacity: 0,
-        stagger: 0.1,
-        ease: 'expo.out'
-      })
-      gsap.from('.title-hidden__gallery', {
-        duration: 2.5,
-        delay: initDelay,
-        opacity: 0,
-        stagger: 0.1,
-        ease: 'expo.out'
-      })
-      gsap.from('.date', {
-        duration: 2.5,
-        opacity: 0,
-        delay: initDelay+3,
-        left: '50%',
-        ease: 'expo.out',
-        onComplete: () => store.commit('completeLandingAnim') // animation完了を通知
-      })
-      gsap.from('.organizer', {
-        duration: 1.5,
-        delay: initDelay,
-        top: '67%',
-        ease: 'expo.out'
-      })
-    })
+    const startTitleAnimation = () => {
+      console.log('start title animation')
+      isConceptAnimDone.value = true
+    }
 
-    return { revealTitles }
+    return { isConceptAnimDone, startTitleAnimation }
   }
 }
 </script>
 
 <style scoped lang='scss'>
 .landing {
-  width: 100%;
   height: 100vh;
-  color: $color-title;
-  .title,
-  .date,
-  .nu-ink {
-    font-weight: 700;
-  }
-  .title {
-    font-size: $font-size-landing-title;
-    &-tsukuba {
-      position: absolute;
-      top: 25%;
-      left: 27%;
-    }
-    &-innovation {
-      position: absolute;
-      top: 38%;
-      left: 39%;
-    }
-    &-gallery {
-      position: absolute;
-      top: 51%;
-      left: 34%;
-    }
-    &-hidden {
-      opacity: 1;
-      width: 100%;
-      height: 100%;
-    }
-  }
-  .date {
-    font-family: $font-family-number;
-    font-size: $font-size-landing-date;
-    position: absolute;
-    top: 67%;
-    left: 56%;
-  }
-  .organizer{ 
-    font-size: 14.5px;
-    margin-top: 45px;
-    position: absolute;
-    top: 83%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    .nu-ink {
-      font-size: $font-size-landing-nuink;
-      .nu {
-        font-family: $font-family-nuink-nu;
-      }
-      .ink {
-        letter-spacing: 1.7px;
-        font-family: $font-family-nuink-ink;
-      }
-    }
-  }
+  text-align: center;
 }
 </style>
