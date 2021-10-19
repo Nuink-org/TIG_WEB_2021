d4da0e7696aceb94e2eacfa6f3ed1bbf356288b0
Jun Nishimura <n.junjun0303@gmail.com>
2021-10-19 18:31:02 +0900

    update projection mapping

--- src/views/contents/ProjectionMappingPage.vue
+++ src/views/contents/ProjectionMappingPage.vue
@@ -22,7 +22,11 @@ export default {
     const videoSrc = "https://www.youtube.com/embed/q48xVf-AbmA"
     const contentGenre = "Projection Mapping"
     const contentTitle = "プロジェクションマッピング"
-    const contentDescriptions = ["プロジェクションマッピングとストリートダンスの融合、その美しさをお伝えします。映像コンテンツでは味わいにくい臨場感を大切にしながら、私たちの思いも詰め込みました。唯一無二の作品となっておりますので、ぜひお楽しみください。"]
+    const contentDescriptions = [
+      "「ダンスとプロジェクションマッピングの融合」をコンセプトに、筑波大学クリエイティブ集団 TParty と共に映像作品を作り上げました。繊細なプロジェクションマッピングやオーディオビジュアルは必見です。", 
+      "企画はNu ink.・制作はTPartyが担い、企画段階を含めると制作に5か月ほど要しました。新型コロナウイルス感染拡大の影響により、撮影延期なども余儀なくされましたが、無事に完成しました。", 
+      "映像の美しさだけでなく、臨場感や込められたメッセージにも注目です。"
+    ]    
     
     return { videoSrc, contentGenre, contentTitle, contentDescriptions }
   }
