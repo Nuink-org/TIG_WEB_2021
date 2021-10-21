706741faac222a53987faaff8824a3a50f245f80
Jun Nishimura <n.junjun0303@gmail.com>
2021-10-21 23:53:34 +0900

    add tparty #55

--- src/views/contents/ProjectionMappingPage.vue
+++ src/views/contents/ProjectionMappingPage.vue
@@ -7,6 +7,21 @@
       :contentDescriptions="contentDescriptions"
     >
       <template #body>
+        <div class="projection-mapping__collaboration">
+          <div class="projection-mapping__collaboration-tparty">
+            <div class="projection-mapping__collaboration-tpartyLogo">
+              <img src="@/assets/images/tparty-logo.png">
+            </div>
+            <div class="projection-mapping__collaboration-tpartyDescription">
+              <div class="projection-mapping__collaboration-tpartyDescription-title">
+                <a href="https://twitter.com/tparty_tsukuba">TParty</a>
+              </div>
+              <div class="projection-mapping__collaboration-tpartyDescription-sentence">
+                テクノロジーを中心にオーディオビジュアル、バイオアート、メディアアート、デザインなど様々な領域で制作活動を行う筑波大学生が集まる団体
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
     </content-page-template>
   </div>
@@ -32,3 +47,40 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.projection-mapping__collaboration {
+  margin-top: 4.32rem;
+  border-top: 1px solid #888;
+  &-tparty {
+    padding-top: 2.67rem;
+    display: flex;
+    justify-content: flex-start;
+    &Logo {
+      width: 120px;
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+    &Description {
+      color: #cecece;
+      width: 100%;
+      margin: 1.618rem 0 1.618rem 1.618rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      &-title {
+        font-size: 16px;
+        a {
+          color: #cecece;
+          text-decoration: none;
+        }
+      }
+      &-sentence {
+        font-size: 14px;
+      }
+    }
+  }
+}
+</style>
