8ac643f071ff08531925b23ec99c4d16b3803b4c
Jun Nishimura <n.junjun0303@gmail.com>
2021-10-22 00:58:17 +0900

    update overview of music experiment #55

--- src/views/contents/MusicExperimentPage.vue
+++ src/views/contents/MusicExperimentPage.vue
@@ -22,7 +22,11 @@ export default {
     const videoSrc = "https://www.youtube.com/embed/q48xVf-AbmA"
     const contentGenre = "Music Experiment"
     const contentTitle = "人工知能と音楽　New Normalな音楽のあり方"
-    const contentDescriptions = ["人工知能を用いて楽曲を制作する自動作曲技術の紹介、実演を行う。情報技術によって創作が身近なものとなっていることを体感できる内容になっている。本イベントのコンセプト「New Normal」のもと新時代の音楽のあり方を紐解く!"]
+    const contentDescriptions = [
+      "“あの世界的アーティストの新曲を人工知能で生成?!”",
+      "近年話題の人工知能の紹介、Open AI Jukeboxを用いた自動作曲のプログラムの実行の様子、生成された曲を披露します。作詞はNu ink.１年生2人が担当、プログラム実行はNu ink.2年生1人が担当しました。そして、完成した曲はあの世界的アーティストの声で再現されます。",
+      "音楽に興味ある方、プログラミングに興味ある方、是非お楽しみください。"
+    ]
     
     return { videoSrc, contentGenre, contentTitle, contentDescriptions }
   }
