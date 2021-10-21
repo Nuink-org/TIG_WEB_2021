9fc876ce16ac286880b68a71ef0c9ab38e86a1b6
Jun Nishimura <n.junjun0303@gmail.com>
2021-10-21 23:56:19 +0900

    add an image for behind the scenes #55

--- src/views/contents/BehindTheScenesPage.vue
+++ src/views/contents/BehindTheScenesPage.vue
@@ -22,7 +22,10 @@ export default {
     const videoSrc = "https://www.youtube.com/embed/q48xVf-AbmA"
     const contentGenre = "Behind The Scenes"
     const contentTitle = "新しい学校のリーダーズ feat. Nu ink."
-    const contentDescriptions = ["10/10に行われたイノフェスでのオープニングアクトの裏側を大公開！新しい学校のリーダーズの本番前の様子やライブを終えてのインタビュー、そしてNu ink.と映像制作メンバーの対談も収録されています！ぜひご覧ください"]
+    const contentDescriptions = [
+      "10月10日に六本木ヒルズアリーナで行われたINNOVATION WORLD FESTA 2021にて、2日目のオープニングアクトを務めた「新しい学校のリーダーズ feat. Nu ink.」のライブステージの舞台裏をまとめました。", 
+      "映像製作やリハーサルの様子を撮影し、ライブステージの演出が完成に至るまでの過程を公開。本番前の楽屋や舞台袖の様子も撮影し、ライブを終えた新しい学校のリーダーズさんにインタビューをさせていただきました。また、映像制作者とNu ink.メンバーによるイノフェスを振り返っての対談も。ぜひご覧ください。"  
+    ]
     
     return { videoSrc, contentGenre, contentTitle, contentDescriptions }
   }
