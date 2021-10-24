8f97e92fb549e0ecc334295c1418148b0c80287a
Jun Nishimura <n.junjun0303@gmail.com>
2021-10-24 20:39:56 +0900

    remove timer from upcoming board #66

--- src/components/contents/UpcomingBoard.vue
+++ src/components/contents/UpcomingBoard.vue
@@ -15,39 +15,13 @@
 
 <script>
 import { useStore } from 'vuex'
-import { ref, onMounted, computed } from 'vue'
+import { computed } from 'vue'
 export default {
   setup() {
     const store = useStore()
     const isResponsivePhone = computed(() => store.state.isResponsivePhone)
-
-    onMounted(() => {
-      setInterval(updateTimer, 1000)
-    })
-
-    const day = ref(0)
-    const hour = ref(0)
-    const min = ref(0)
-    const sec = ref(0)
     
-    const updateTimer = (() => {
-      const curTime = new Date()
-      const targetTime = new Date('2021-11-06T00:00:00')
-      const diff = targetTime.getTime() - curTime.getTime()
-      day.value = diff / (1000 * 60 * 60 * 24)
-      hour.value = (day.value - Math.floor(day.value)) * 24
-      min.value = (hour.value - Math.floor(hour.value)) * 60
-      sec.value = (min.value - Math.floor(min.value)) * 60
-    })
-
-    const displayTime = computed(() => {
-      return Math.floor(day.value).toString() + '日'
-        + Math.floor(hour.value).toString() + '時間'
-        + Math.floor(min.value).toString() + '分'
-        + Math.floor(sec.value).toString() + '秒'
-    })
-    
-    return { displayTime, isResponsivePhone }
+    return { isResponsivePhone }
   }
 }
 </script>
